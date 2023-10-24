62db0a327cb224814fba57f137650a5d91214153
Mohit Saini <mailtomohit2002@gmail.com>
2023-10-24 21:10:23 +0530

    30 - Login via Django AuthenticationForm

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
 from django.contrib.auth import authenticate, login , logout
 
 # register view 
@@ -13,20 +13,29 @@ def register_view(request):
 
 # Create your views here.
 def login_view(request):
-    if request.method == "POST":
-            username = request.POST.get("username")
-            password = request.POST.get("password")
-            print(username,password)
-            user = authenticate(request, username=username,password = password)
-            if user is None:
-                context = {"error" : "Invalid Username or password"}
-                return render(request, "accounts/login.html",context)
+    # form = AuthenticationForm(request.POST or None)
+    # if request.method == "POST":
+    #         username = request.POST.get("username")
+    #         password = request.POST.get("password")
+    #         print(username,password)
+    #         user = authenticate(request, username=username,password = password)
+    #         if user is None:
+    #             context = {"error" : "Invalid Username or password"}
+    #             return render(request, "accounts/login.html",context)
             
+    #         login(request,user)
+    #         return redirect('/admin')
+    if request.method == "POST":
+        form = AuthenticationForm(request,data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
             login(request,user)
-            return redirect('/admin')
+            return redirect('/')
+    else :
+        form = AuthenticationForm(request)
+    context = {"form" : form}
 
-    
-    return render(request, "accounts/login.html", {})
+    return render(request, "accounts/login.html", context)
 
 def logout_view(request):
     if request.method =="POST": 
