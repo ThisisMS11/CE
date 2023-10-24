73a29b4f9b116648e68c8241e727eee0c4a15964
Mohit Saini <mailtomohit2002@gmail.com>
2023-10-24 20:52:37 +0530

    29 - Register a User via built in Model Form

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login , logout
+
+# register view 
+def register_view(request):
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        user_obj = form.save()
+        return redirect('/login')
+    context = {"form":form}
+    return render(request,"accounts/register.html",context)
+
 # Create your views here.
 def login_view(request):
     if request.method == "POST":
@@ -23,5 +34,3 @@ def logout_view(request):
         return redirect('/login/')
     return render(request, "accounts/logout.html", {})
 
-def register_view(request):
-    return render(request, "accounts/register.html", {})
